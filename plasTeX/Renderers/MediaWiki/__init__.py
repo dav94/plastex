5ed19c90ab733d0c818e8292c2a896dde08c1217
Davide Valsecchi <valsecchi.davide94@gmail.com>
2015-09-26 22:24:59 +0200

    Creation of MediaWiki Markup Renderer

---
+++ plasTeX/Renderers/MediaWiki/__init__.py
@@ -0,0 +1,130 @@
+import string,re
+from plasTeX.Renderers import Renderer
+
+class MediaWikiRenderer (Renderer):
+
+    outputType = unicode
+    fileExtension = '.xml'
+    lineWidth = 76
+
+    last_section = ''
+
+    def __init__(self, *args, **kwargs):
+        Renderer.__init__(self, *args, **kwargs)
+        # Load dictionary with methods
+        for key in dir(self):
+            if key.startswith('do_'):
+                self[key[3:]] = getattr(self, key)
+        self['default-layout'] = self['document-layout'] = self.default
+        self.footnotes = []
+        self.blocks = []
+
+    def default(self, node):
+        s = []
+        s.append('<%s>' % node.nodeName)
+        s.append(unicode(node))
+        s.append('</%s>' % node.nodeName)
+        return u''.join(s)
+
+    def do_equation(self, node):
+    	s = []
+        s.append('<dmath>')
+        #child nodes
+        s.append(unicode(node))
+        #endtah
+        s.append('</dmath>')
+        return u''.join(s)
+
+    def do_textDefault(self, node):
+        return node  
+
+    def do_document(self,node):
+        content = unicode(node)
+        return u'%s' % content
+
+    def do_par(self, node):
+        s = []
+        s.append(u'par')
+        s.append(unicode(node))
+        s.append(u'end_par')
+        return u''.join(s)
+        
+    def do_textbf(self,node):
+        s=[]
+        s.append(u"\'\'")
+        s.append(unicode(node))
+        s.append(u"\'\'")
+        return u''.join(s)
+        
+    def do_textit(self,node):
+        s=[]
+        s.append(u"\'\'\'")
+        s.append(unicode(node))
+        s.append(u"\'\'\'")
+        return u''.join(s)    
+
+    do_emph = do_textbf
+
+    def do_section(self, node):
+        self.last_section = node.attributes["title"]
+    	s=[]
+    	s.append(u'[[')
+    	s.append(u'%s' % self.last_section)
+    	s.append(u']]')
+    	s.append(unicode(node))
+    	return u''.join(s)
+
+    def do_subsection(self,node):
+    	s=[]
+    	s.append(u'[[')
+    	s.append(u'%s' % (self.last_section+"/"+ node.attributes["title"]))
+    	s.append(u']]')
+    	s.append(unicode(node))
+    	return u''.join(s)
+
+
+
+class XMLRenderer(Renderer):
+
+
+    def __init__(self, *args, **kwargs):
+        Renderer.__init__(self, *args, **kwargs)
+        # Load dictionary with methods
+        for key in dir(self):
+            if key.startswith('do_'):
+                self[key[3:]] = getattr(self, key)
+        self['default-layout'] = self['document-layout'] = self.default
+        self.footnotes = []
+        self.blocks = []
+        self['\\']=self.backslash
+    
+
+    def default(nself,node):
+        s = []
+        s.append('<%s>' % node.nodeName)
+        if node.hasAttributes() and len(node.attributes)>1:
+            s.append('<attributes>')
+            for key, value in node.attributes.items():
+                if key == 'self':
+                    continue
+                s.append('<%s>%s</%s>' % (key,unicode(value),key))
+            s.append('</attributes>')
+
+        s.append(unicode(node))
+        s.append('</%s>\n' % node.nodeName)
+        return u'\n'.join(s)
+
+    def textDefault(self,node):
+        return unicode(node)
+
+    def backslash(self,node):
+        return u"<accapo>"
+
+    def do_math(self, node): #TBD
+        return '<math>'+re.sub(r'\s*(_|\^)\s*', r'\1', node.source)+'</math>'
+
+    do_ensuremath = do_math
+    
+    def do_equation(self, node): #TBD
+        s = u'   %s' % re.compile(r'^\s*\S+\s*(.*?)\s*\S+\s*$', re.S).sub(r'\1', node.source)
+        return '<math>'+re.sub(r'\s*(_|\^)\s*', r'\1', s)+'</math>'
